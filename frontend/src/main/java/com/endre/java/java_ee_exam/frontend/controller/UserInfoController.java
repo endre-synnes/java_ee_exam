c277a275c40afe516d256e5bcdf858c639a41052
synend16 <synend16@student.westerdals.no>
2018-04-26 23:02:18 +0200

    added all files nessesary but not working

---
+++ frontend/src/main/java/com/endre/java/java_ee_exam/frontend/controller/UserInfoController.java
@@ -0,0 +1,19 @@
+package com.endre.java.java_ee_exam.frontend.controller;
+
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class UserInfoController {
+
+
+    public String getUserName(){
+        return ((UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+    }
+
+}
