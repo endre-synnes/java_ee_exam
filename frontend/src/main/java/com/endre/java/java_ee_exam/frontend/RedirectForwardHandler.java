f5d2609f30e2a2145d0c370842a9ca469d2b695f
synend16 <synend16@student.westerdals.no>
2018-05-01 20:33:52 +0200

    no real change

--- frontend/src/main/java/com/endre/java/java_ee_exam/frontend/RedirectForwardHandler.java
+++ frontend/src/main/java/com/endre/java/java_ee_exam/frontend/RedirectForwardHandler.java
@@ -1,12 +1,7 @@
 package com.endre.java.java_ee_exam.frontend;
 
-import org.springframework.boot.context.config.ResourceNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RedirectForwardHandler {
